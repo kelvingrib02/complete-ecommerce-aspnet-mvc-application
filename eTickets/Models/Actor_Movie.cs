4f4b70c4c0f9841c901fa676237fa36ab4350219
Kelvin Gabriel  Ribeiro <kelvin@twrt.com.br>
2023-09-04 22:55:25 -0300

    Configurando banco

---
+++ eTickets/Models/Actor_Movie.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor_Movie
+    { 
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        public int Actor_Id { get; set; }
+        public Actor Actor { get; set; }
+    }
+}
